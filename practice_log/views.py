4bb3b6c2a3bfb93574328f17c638aee0a91682b7
Ed Denham <ed.denham@gmail.com>
2023-02-20 01:12:11 +0000

    Create base template and add url and view

--- practice_log/views.py
+++ practice_log/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+
+class Dashboard(TemplateView):
+
+    template_name = "base.html"
+
+    # def get(self, request, *args, **kwargs):
+    #     return HttpResponse('Hello, World')
