a8e8dfcde3afadff032a13a9a1a1c0aca2f8abe6
Ed Denham <ed.denham@gmail.com>
2023-02-22 21:56:17 +0000

    Create StudentProfile model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    goals = JSONField(default=list)
+    resources = models.ManyToManyField(
+        Article,
+        related_name='student_resources',
+        blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
